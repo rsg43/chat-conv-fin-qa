ed308549d785853d6c778ddacaee6e79b84cb62a
rsg43 <ravindag@hotmail.com>
2025-05-20 12:54:10 +0000

    add basic model clients

---
+++ chat_conv_fin_qa/model/openai.py
@@ -0,0 +1,22 @@
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai.chat_models import ChatOpenAI
+
+from chat_conv_fin_qa.model.base import BaseModel
+
+
+class AnthropicModel(BaseModel):
+
+    def __init__(self):
+        self._model = ChatOpenAI(
+            model="gpt-4o",
+            temperature=0.3,
+            max_tokens=1024,
+            timeout=60,
+            max_retries=3,
+        )
+
+    def chat(self, messages: list[BaseMessage]) -> BaseMessage:
+        return self._model.invoke(input=messages)
+
+    def invoke(self, prompt: str) -> str:
+        return self._model.invoke(input=[HumanMessage(content=prompt)]).content
