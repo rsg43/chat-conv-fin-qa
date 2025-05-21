d6d5a4a59edd1c2c719449b1a32ae9e5fb6939ac
rsg43 <ravindag@hotmail.com>
2025-05-21 14:45:01 +0000

    move logic to base model for model apis

--- chat_conv_fin_qa/model/openai.py
+++ chat_conv_fin_qa/model/openai.py
@@ -1,4 +1,3 @@
-from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 from langchain_openai.chat_models import ChatOpenAI
 
 from chat_conv_fin_qa.model.base import BaseModel
@@ -6,17 +5,10 @@ from chat_conv_fin_qa.model.base import BaseModel
 
 class AnthropicModel(BaseModel):
 
-    def __init__(self) -> None:
-        self._model = ChatOpenAI(
-            model="gpt-4o",
-            temperature=0.3,
-            max_tokens=1024,
-            timeout=60,
-            max_retries=3,
-        )
-
-    def chat(self, messages: list[BaseMessage]) -> AIMessage:
-        return self._model.invoke(input=messages)
-
-    def invoke(self, prompt: str) -> str:
-        return self._model.invoke(input=[HumanMessage(content=prompt)]).content
+    _model = ChatOpenAI(
+        model="gpt-4o",
+        temperature=0.3,
+        max_tokens=1024,
+        timeout=60,
+        max_retries=3,
+    )
