d0afbd903802bc4534282ba2d97ef88b7750e25d
rsg43 <ravindag@hotmail.com>
2025-05-23 08:00:16 +0000

    model docstrings

--- chat_conv_fin_qa/model/anthropic.py
+++ chat_conv_fin_qa/model/anthropic.py
@@ -1,9 +1,17 @@
+"""
+Module for Anthropic model integration. uses Clause 3 via LangChain Anthropic
+chat model wrapper.
+"""
+
 from langchain_anthropic.chat_models import ChatAnthropic
 
 from chat_conv_fin_qa.model.base import BaseModel
 
 
 class AnthropicModel(BaseModel):
+    """
+    Anthropic model wrapper for LangChain Anthropic chat model.
+    """
 
     _model = ChatAnthropic(
         model="claude-3-haiku-20240307",
